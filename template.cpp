024b704fc3c15b111c9b113ed96188517b69b313
s-nandi <saujas.nandi@gmail.com>
2016-10-17 15:32:54 -0400

    Added template file
    Contains header files and macros

---
+++ template.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <set>
+#include <utility>
+#include <stack>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+#include <sstream>
+#include <cstdlib>
+
+#define REP(i, x) for(int i = 0; i < x; i++)
+#define REPS(i, s, x) for(int i = s; i < x; i++)
+#define newl '\n'
+#define dbg cerr
+#define ll long long
+#define ii pair<int, int>
+#define iii pair <ii, int>
+#define iiii pair<ii, ii>
+#define vi vector <int>
+#define vii vector <ii>
+#define vs vector <string>
+#define PB push_back
+#define INF 1000000008
+#define all(x) x.begin(), x.end()
+
+using namespace std;
+
+int main()
+{
+    //freopen("/Users/Mac/Desktop/input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+
+    return 0;
+}
