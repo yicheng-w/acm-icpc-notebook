b25863fad2313cead37cf29e15d3963e69807d9d
s-nandi <saujas.nandi@gmail.com>
2016-11-05 00:37:58 -0400

    Update sieve.cpp

--- general-algorithm/sieve.cpp
+++ general-algorithm/sieve.cpp
@@ -5,13 +5,13 @@ vector <int> sieve(int n)
   int visited[n];
   vector <int> sol;
   memset(visited, 0, sizeof(visited));
-  for (int i = 2; i * i < n; i ++)
+  for (int i = 2; i * i < n; i++)
   {
     if (visited[n] == 0)
     {
       sol.push_back(i);
     }
-    for (int j = i * i; j < n; j+=i)
+    for (int j = i * i; j < n; j += i)
     {
       visited[j] = 1;
       
